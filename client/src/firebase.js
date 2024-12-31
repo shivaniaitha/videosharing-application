532d44cbe3d958ee95a992d3bfdc1e12d421af18
shivaniaitha <113002046+shivaniaitha@users.noreply.github.com>
2024-12-31 12:31:45 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDaRz1GIXrS-oeR4pMgtu15HONa7FDiZHI",
-  authDomain: "clone-1f8c4.firebaseapp.com",
-  projectId: "clone-1f8c4",
-  storageBucket: "clone-1f8c4.appspot.com",
-  messagingSenderId: "472167000339",
-  appId: "1:472167000339:web:672fbacdf8acc78e1c92e6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage();
+export const storage = getStorage(app);
